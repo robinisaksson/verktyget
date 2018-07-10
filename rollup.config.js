ceb8f781f4e34c5bc3f67b51c2b36618065ffdd6
Robin <robin.isaksson@gmail.com>
2018-07-10 12:31:32 +0200

    Test build

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,28 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
-import istanbul from 'rollup-plugin-istanbul';
 
-// TODO
-// babel-preset-es2015@6.24.1: 🙌  Thanks for using Babel: we recommend using babel-preset-env now: please read babeljs.io/env to update! 
-
-let pkg = require('./package.json');
-let external = Object.keys(pkg.dependencies);
-
-let plugins = [
-  babel(babelrc()),
-];
-
-if (process.env.BUILD !== 'production') {
-  plugins.push(istanbul({
-    exclude: ['test/**/*', 'node_modules/**/*']
-  }));
-}
+const babelConfig = {
+  'presets': [
+    ['env', {
+      'targets': {
+        'browsers': ['last 2 versions']
+      },
+      'loose': true
+    }]
+  ]
+};
 
 export default {
-  entry: 'lib/index.js',
-  plugins: plugins,
-  external: external,
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: 'verktyget',
-      sourceMap: true
-    },
-    {
-      dest: pkg.module,
-      format: 'es',
-      sourceMap: true
-    }
-  ]
+  entry: 'src/alpha.js',
+  format: 'umd',
+  moduleName: 'verktyget',
+  sourceMap: true,
+  plugins: [
+    babel(babelrc({
+      addExternalHelpersPlugin: false,
+      config: babelConfig,
+      exclude: 'node_modules/**'
+    }))
+  ],
+  dest: 'dist/bundle.js'
 };
