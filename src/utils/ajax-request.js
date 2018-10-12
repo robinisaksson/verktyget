5416158e03f313c5d796d6510674f43aefa853e4
Robin <robin.isaksson@gmail.com>
2018-10-12 14:31:47 +0200

    Clean up ajax-request

--- src/utils/ajax-request.js
+++ src/utils/ajax-request.js
@@ -2,15 +2,15 @@ import {EventDispatcher} from './event-dispatcher';
 
 export class AjaxRequest extends EventDispatcher {
 
-  // public static TYPE_GET:string = "GET";
-  // public static TYPE_POST:string = "POST";
-
 	constructor(url) {
 
     super();
-
-    this.request; //:XMLHttpRequest;
-    this.requestFormat = "application/x-www-form-urlencoded"; //application/json
+		
+		this.onLoadComplete = this.onLoadComplete.bind(this);
+		this.onProgress = this.onProgress.bind(this);
+		
+    this.request;
+    this.requestFormat = 'application/x-www-form-urlencoded'; //application/json
     this.progress = 0;
 
 		this.url = url;
@@ -25,33 +25,23 @@ export class AjaxRequest extends EventDispatcher {
 
   load() {
 
-    console.log('Load: ', this.url);
-
-    // regular HTTP request
+    // Regular HTTP request
     this.request = new XMLHttpRequest();
 
-    // listen for async request state change
-    this.request.onreadystatechange = this.onLoadCompleteHandler.bind(this);
-    this.request.onprogress = this.onProgressHandler.bind(this);
-    // this.onProgressHandler = this.onProgressHandler.bind(this);
-    // this.request.addEventListener("progress", this.onProgressHandler);
+    // Listen for async request state change
+    this.request.onreadystatechange = this.onLoadComplete;
+    this.request.onprogress = this.onProgress;
+    // this.request.addEventListener("progress", this.onProgress);
 
     var url = this.url;
-    var requestDataString = ''; //URLUtility.URLEncodeObject(this.requestData);
-
-    // if GET
-    // if(this.requestType === 'GET') {
-    //   //each variable has to be encodeURIComponent(value)
-    //   url += (url.indexOf("?") === -1 ? "?" : "&")+requestDataString;
-    //   requestDataString = null;
-    // }
+    var requestDataString = '';
 
     this.request.open(this.requestType, url, true);
-    this.request.setRequestHeader("Content-Type", this.requestFormat);//"application/x-www-form-urlencoded");
+    this.request.setRequestHeader('Content-Type', this.requestFormat); // 'application/x-www-form-urlencoded'
 
-    if (this.requestFormat === "application/x-www-form-urlencoded") {
+    if (this.requestFormat === 'application/x-www-form-urlencoded') {
       this.request.send(requestDataString);
-    } else if (this.requestFormat === "application/json") {
+    } else if (this.requestFormat === 'application/json') {
       this.request.send(JSON.stringify(this.requestData));
     }
   }
@@ -60,7 +50,7 @@ export class AjaxRequest extends EventDispatcher {
   kill() {
     this.request.onreadystatechange = null;
     this.request.onprogress = null;
-    // this.request.removeEventListener("progress", this.onProgressHandler);
+    // this.request.removeEventListener("progress", this.onProgress);
   }
 
   destroy() {
@@ -73,7 +63,7 @@ export class AjaxRequest extends EventDispatcher {
   }
 
 
-  onProgressHandler(event) {
+  onProgress(event) {
     if (event.lengthComputable === true) {
       this.progress = event.loaded / event.total;
       this.dispatchEvent({type:'progress', target:this});
@@ -81,37 +71,29 @@ export class AjaxRequest extends EventDispatcher {
   }
 
 
-  onLoadCompleteHandler(e) {
+  onLoadComplete(e) {
 
     if (this.request.readyState === 4) {
 
       if (this.request.status >= 200 && this.request.status < 400) {
-
+				
         // Store html response
         this.htmlResponse = this.request.responseText;
 
         // Clear loader
         this.request.onreadystatechange = null;
         this.request.onprogress = null;
-        // this.request.removeEventListener('progress', this.onProgressHandlerBind);
+        // this.request.removeEventListener('progress', this.onProgress);
 
-        // Dispatch event, this must be the last call
-        this.dispatchEvent({type:'complete', target:this});
+        // Dispatch event 'loaded' event
+        this.dispatchEvent({type:'loaded', target:this});
 
       } else if (this.request.status > 400 || this.request.status === 0) {
-          // error...
-
-          this.dispatchEvent({type:'error', target:this});
-
+        this.dispatchEvent({type:'error', target:this});
       }
     }
   }
 
-  onCompleteHandler(e) {
-    // to be overwritten by the sub class
-    console.log("on complete!");
-    console.log(e);
-  }
 
 }
 
